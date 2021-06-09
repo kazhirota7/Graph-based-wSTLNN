84edc71c7a346c0e2cdff601d837f1cc8661171c
kazhirota7 <khrealmadrid@gmail.com>
2021-06-08 20:27:09 -0500

    a

--- wSTLNN/wSTLNN/Model.py
+++ wSTLNN/wSTLNN/Model.py
@@ -3,23 +3,42 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 
 class TL_NN(nn.Module):
-    def __init__(self, T, M):
+    def __init__(self, T, M, n):
         super(TL_NN,self).__init__()
-        self.t = torch.nn.Parameter(1e-5 * torch.ones(M,1), requires_grad=True)
+        self.t = torch.nn.Parameter(1e-5 * torch.ones(M,1), requires_grad=True)  # coefficients for inputs (s) (5, 1) (number of inputs for each node)
         A = torch.rand((T,1),requires_grad=True)
-        self.A = torch.nn.Parameter(A)
-        self.b = torch.nn.Parameter(torch.randn(1,1), requires_grad=True)
+        self.A = torch.nn.Parameter(A)  # weights (16, 1)
+        self.b = torch.nn.Parameter(torch.randn(1,1), requires_grad=True)  # bias for time
+        self.n = n
+
+
     def forward(self,x):
-         r_a = torch.matmul(x, self.t) - self.b
-         self.r_a =  r_a.reshape((-1,16)).t()
-         self.A_abs = torch.abs(self.A)
-         self.x_sftmin = F.softmin(self.r_a, 0)
-         self.A_sm =  self.A_abs / torch.sum(self.A_abs)
-         self.A_sm_nonzr = self.A_sm
-         self.wsx = self.A_sm_nonzr * self.x_sftmin * self.r_a
-         self.weisum = torch.sum(self.A_sm_nonzr * self.x_sftmin, 0)
-         self.xrtn = torch.sum(self.wsx, 0) / self.weisum
-         return self.xrtn
+        # print(np.shape(x))
+        r_a = torch.matmul(x, self.t) - self.b  # [numData, 16, n+1, 1]
+        self.r_a = r_a.reshape(-1, 16, self.n+1).permute((1, 0, 2))
+        self.grph_sftmax = F.softmax(self.r_a, 2)  # [16, numData, n+1] - output shape
+        self.grph_sftmin = F.softmin(self.r_a, 2)  # [16, numData, n+1] - output shape
+        # print(np.shape(self.r_a))
+        self.r_a = self.r_a.reshape(16, -1)
+        self.grph_sftmax = self.grph_sftmax.reshape((16, -1))  # [16, numData * (n+1)] = output shape
+        self.grph_sftmin = self.grph_sftmin.reshape((16, -1))  # [16, numData * (n+1)] = output shape
+        # print(np.shape(self.grph_sftmax))
+        self.tmp_sftmin = F.softmin(self.grph_sftmax, 0)  # [16, numData * (n+1)]
+        self.tmp_sftmax = F.softmax(self.grph_sftmin, 0)  # [16, numData * (n+1)]
+        # print(np.shape(self.tmp_sftmax))
+        self.x_sftmax = torch.max(self.tmp_sftmin, self.tmp_sftmax)  # [16, numData * (n+1)]
+        # print(np.shape(self.x_sftmax))
+        self.A_abs = torch.abs(self.A)
+        self.A_sm = self.A_abs / torch.sum(self.A_abs)
+        self.A_sm_nonzr = self.A_sm
+        self.wsx = self.A_sm_nonzr * self.x_sftmax * self.r_a
+        # print(np.shape(self.wsx))
+        self.weisum = torch.sum(self.A_sm_nonzr * self.x_sftmax, 0)
+        self.xrtn = torch.sum(self.wsx, 0) / self.weisum
+        # print(np.shape(self.xrtn))
+        print([self.t, self.A, self.b])
+        return self.xrtn
--- wSTLNN/wSTLNN/wSTLNN.py
+++ wSTLNN/wSTLNN/wSTLNN.py
@@ -10,9 +10,9 @@ from torch.autograd import Variable
 import time
 from Model import TL_NN
 
-
 torch.manual_seed(223626)
 np.random.seed(223626)
+
 
 # test accuracy
 def Test_accu(tl_nn, X_test, y_test):
@@ -21,29 +21,32 @@ def Test_accu(tl_nn, X_test, y_test):
     accu = np.sum(Xpred_sign == y_test) / len(y_test)
     return Xpred_sign, accu
 
+
 # load data
 def Read_data(train_path, test_path):
     df = pd.read_csv(train_path)
     data_test = pd.read_csv(test_path)
     return df, data_test
 
+
 # dataframe to data and label
 def Dataframe_to_Xy(df, data_len):
     labelcol = df['Occupancy'].to_list()
-    c1 = df.iloc[:,1:6].to_numpy()
+    c1 = df.iloc[:, 1:6].to_numpy()
     X = []
     y = []
-    
+
     i = 0
     while i < len(labelcol):
         cur_label = labelcol[i]
-        label_dur = labelcol[i:i+data_len]
+        label_dur = labelcol[i:i + data_len]
         dif_sign = len(set(label_dur))
         if dif_sign == 1 and {cur_label} == set(label_dur):
-            X.append(c1[i:i+data_len,:].astype(float))
+            X.append(c1[i:i + data_len, :].astype(float))
             y.append(cur_label)
         i = i + data_len
     return X, y
+
 
 # Training and test data, label
 def GetXy(train_path, test_path, data_len):
@@ -52,15 +55,18 @@ def GetXy(train_path, test_path, data_len):
     Xtest, ytest = Dataframe_to_Xy(test_df, data_len)
     return X, y, Xtest, ytest
 
+
 def SlideXy(X, y, Xtest, ytest):
     X, y = X[:-1], y[:-1]
     Xtest, ytest = Xtest[:-1], ytest[:-1]
     return X, y, Xtest, ytest
 
+
 def Xtoarray(X, Xtest):
-    X_stack = np.stack(X,0)
-    Xtest = np.stack(Xtest,0)
+    X_stack = np.stack(X, 0)
+    Xtest = np.stack(Xtest, 0)
     return X_stack, Xtest
+
 
 def Changelabel(ytest, y, X):
     ytest0_pos = [i for i in range(len(ytest)) if ytest[i] == 0]
@@ -77,72 +83,95 @@ def Changelabel(ytest, y, X):
     y10dif_len = y0_len - y1_len
     for i in range(y10dif_len):
         idx = np.random.randint(y1_len)
-        newx = X[y1_pos[idx]] + np.random.randn(16,5)
-        X = np.concatenate((X,newx.reshape((1,16,5))))
+        newx = X[y1_pos[idx]] + np.random.randn(16, 5)
+        X = np.concatenate((X, newx.reshape((1, 16, 5))))
         y.append(1)
     y = np.array(y)
     return X, y, ytest
 
+
 # Split data into training and test
-def Splitdata(train_path, test_path, data_len):
-    X, y, Xtest, ytest = GetXy(train_path, test_path, data_len)
-    X, y, Xtest, ytest = SlideXy(X, y, Xtest, ytest)
-    X, y, ytest = Changelabel(ytest, y, X)
-    (N, T, M)  = np.shape(X)
-    num = list(range(N))
-    random.shuffle(num)
-    X_perm, y_perm = X[np.array(num),:,:], y[np.array(num)]
-    X, y = X_perm, y_perm
-    X_train, y_train = X, y
-    X_total = np.concatenate((X_train, Xtest))
-    y_total = np.concatenate((y_train, np.array(ytest)))
-    N_new = len(X_total)
-    N_train = int(0.8 * N_new)
-    X_train, y_train = X_total[:N_train,:,:], y_total[:N_train]
-    Xtest, ytest = X_total[N_train:,:,:], y_total[N_train:]
-    return X_train, y_train, Xtest, ytest, T, M
+def Splitdata(train_paths, test_paths, data_len):
+    X_train_tot = []
+    y_train_tot = []
+    Xtest_tot = []
+    ytest_tot = []
+    n = len(train_paths) - 1  # number of neighbors
+    for i in range(len(train_paths)):  # append for each node
+        X, y, Xtest, ytest = GetXy(train_paths[i], test_paths[i], data_len)
+        X, y, Xtest, ytest = SlideXy(X, y, Xtest, ytest)
+        X, y, ytest = Changelabel(ytest, y, X)
+        (N, T, M) = np.shape(X)
+        num = list(range(N))
+        random.shuffle(num)
+        X_perm, y_perm = X[np.array(num), :, :], y[np.array(num)]
+        X, y = X_perm, y_perm
+        X_train, y_train = X, y
+        X_total = np.concatenate((X_train, Xtest))
+        y_total = np.concatenate((y_train, np.array(ytest)))
+        N_new = len(X_total)
+        N_train = int(0.8 * N_new)
+        X_train, y_train = X_total[:N_train, :, :], y_total[:N_train]
+        Xtest, ytest = X_total[N_train:, :, :], y_total[N_train:]
+        X_train_tot.append(X_train)
+        y_train_tot.append(y_train)
+        Xtest_tot.append(Xtest)
+        ytest_tot.append(ytest)
+    # print(X_train_tot)
+    # print(np.shape(X_train_tot))
+    X_train_tot = np.reshape(X_train_tot, (N_train, T, n+1, M))  # (batch, time, node, input)
+    # print(np.shape(X_train_tot))
+    # print(np.shape(ytest))
+    y_train_tot = np.transpose(np.reshape(y_train_tot, (-1, )))
+    Xtest_tot = np.reshape(Xtest_tot, (N_new-N_train, T, n+1, M))
+    ytest_tot = np.transpose(np.reshape(ytest_tot, (-1, )))
+
+    # y_train_tot = np.reshape(y_train_tot, ())
+    # print(X_train_tot)
+    # print(np.shape(Xtest))
+    return X_train_tot, y_train_tot, Xtest_tot, ytest_tot, T, M, n
 
 
 def Plotdata(X, y):
     plt.figure()
     for i in range(len(y)):
         if y[i] == -1:
-            plt.plot(X[i,:],'g')
+            plt.plot(X[i, :], 'g')
         else:
-            plt.plot(X[i,:], 'r')
+            plt.plot(X[i, :], 'r')
     plt.show()
+
 
 # Evaluate classification performance
 def Evaluate_measure(tp, fp, tn, fn):
-    sensitivity = tp/(tp+fn)
-    specificity = tn/(fp+tn)
-    positive_pred_value = tp/(tp+fp)
-    negative_pred_value = tn/(tn+fn)
-    auc = (sensitivity + specificity)/2
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (fp + tn)
+    positive_pred_value = tp / (tp + fp)
+    negative_pred_value = tn / (tn + fn)
+    auc = (sensitivity + specificity) / 2
     return sensitivity, specificity, positive_pred_value, negative_pred_value, \
-            auc
+           auc
 
 
 def main():
-    train_path = "occupancy_data/datatraining.txt"
-    test_path = "occupancy_data/datatest.txt"
+    train_paths = ["occupancy_data/datatraining.txt"]
+    test_paths = ["occupancy_data/datatest.txt"]
     data_len = 16
-    X_train, y_train, Xtest, ytest, T, M = Splitdata(train_path, test_path, data_len)
+    X_train, y_train, Xtest, ytest, T, M, n = Splitdata(train_paths, test_paths, data_len)
     train_size = len(X_train)
-    Plotdata(X_train, y_train)
-    print(T, M)
-    tl_nn = TL_NN(T, M)
+    # Plotdata(X_train, y_train)
+    tl_nn = TL_NN(T, M, n)
     learning_rate = 0.001
-    optimizer = torch.optim.RMSprop(tl_nn.parameters(), lr = learning_rate)
+    optimizer = torch.optim.RMSprop(tl_nn.parameters(), lr=learning_rate)
     batch_size = 16
     Epoch = 10
     loss_iter = []
     accu_iter = []
     Perfor_iter = []
     for epoch in range(Epoch):
-        for d_i in range(train_size//batch_size + 1):
+        for d_i in range(train_size // batch_size + 1):
             rand_idx = np.random.randint(0, train_size, batch_size)
-            X_bt = Variable(torch.Tensor(X_train[rand_idx,:,:]))
+            X_bt = Variable(torch.Tensor(X_train[rand_idx, :, :, :]))
             y_bt = Variable(torch.LongTensor(y_train[rand_idx,]))
             X_btpred = tl_nn(X_bt)
             loss_tlnn = torch.sum(torch.exp(-y_bt * X_btpred))
@@ -150,11 +179,13 @@ def main():
             loss_tlnn.backward()
             optimizer.step()
             loss_iter.append(loss_tlnn.detach().numpy())
-            
+
             if d_i % 2 == 0:
                 pred_sign, test_accu = Test_accu(tl_nn, Xtest, ytest)
+                print(pred_sign)
+                print(ytest)
                 accu_iter.append(test_accu)
-                tp,  fp, tn, fn = 0,0,0,0
+                tp, fp, tn, fn = 0, 0, 0, 0
                 for i in range(len(pred_sign)):
                     pdlb = pred_sign[i]
                     aclb = ytest[i]
@@ -166,15 +197,14 @@ def main():
                         fn += 1
                     elif pdlb == -1 and aclb == -1:
                         tn += 1
-                Perfor_iter.append([tp,fp,tn,fn])
+                Perfor_iter.append([tp, fp, tn, fn])
 
-    sensitivity, specificity, positive_pred_value, negative_pred_value, auc \
-    = Evaluate_measure(tp, fp, tn, fn)
+    #sensitivity, specificity, positive_pred_value, negative_pred_value, auc \
+    #    = Evaluate_measure(tp, fp, tn, fn)
     print('Accuracy is {}%'.format(test_accu))
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
 
 
