6127412f794daef4c604a9cc605a4b826e610f2a
kazhirota7 <khrealmadrid@gmail.com>
2021-06-11 08:17:04 -0500

    updated: 6/11/2021

--- wSTLNN/wSTLNN/Model.py
+++ wSTLNN/wSTLNN/Model.py
@@ -14,23 +14,27 @@ class TL_NN(nn.Module):
         self.A = torch.nn.Parameter(A)  # weights (16, 1)
         self.b = torch.nn.Parameter(torch.randn(1,1), requires_grad=True)  # bias for time
         self.n = n
+        self.t2 = torch.nn.Parameter(1e-5 * torch.ones(n+1,1), requires_grad=True)
+        self.b2 = torch.nn.Parameter(torch.randn(1,1), requires_grad=True)
 
 
     def forward(self,x):
         # print(np.shape(x))
         r_a = torch.matmul(x, self.t) - self.b  # [numData, 16, n+1, 1]
         self.r_a = r_a.reshape(-1, 16, self.n+1).permute((1, 0, 2))
-        self.grph_sftmax = F.softmax(self.r_a, 2)  # [16, numData, n+1] - output shape
-        self.grph_sftmin = F.softmin(self.r_a, 2)  # [16, numData, n+1] - output shape
+        grph_sftmax = F.softmax(self.r_a, 2)  # [16, numData, n+1] - output shape
+        grph_sftmin = F.softmin(self.r_a, 2)  # [16, numData, n+1] - output shape
+        self.grph_sftmax = torch.matmul(grph_sftmax, self.t2) - self.b2  # [16, numData, 1]
+        self.grph_sftmin = torch.matmul(grph_sftmin, self.t2) - self.b2  # [16, numData, 1]
         # print(np.shape(self.r_a))
         self.r_a = self.r_a.reshape(16, -1)
-        self.grph_sftmax = self.grph_sftmax.reshape((16, -1))  # [16, numData * (n+1)] = output shape
-        self.grph_sftmin = self.grph_sftmin.reshape((16, -1))  # [16, numData * (n+1)] = output shape
+        self.grph_sftmax = self.grph_sftmax.reshape((16, -1))  # [16, numData] = output shape
+        self.grph_sftmin = self.grph_sftmin.reshape((16, -1))  # [16, numData] = output shape
         # print(np.shape(self.grph_sftmax))
-        self.tmp_sftmin = F.softmin(self.grph_sftmax, 0)  # [16, numData * (n+1)]
-        self.tmp_sftmax = F.softmax(self.grph_sftmin, 0)  # [16, numData * (n+1)]
+        self.tmp_sftmin = F.softmin(self.grph_sftmax, 0)  # [16, numData]
+        self.tmp_sftmax = F.softmax(self.grph_sftmin, 0)  # [16, numData]
         # print(np.shape(self.tmp_sftmax))
-        self.x_sftmax = torch.max(self.tmp_sftmin, self.tmp_sftmax)  # [16, numData * (n+1)]
+        self.x_sftmax = torch.max(self.tmp_sftmin, self.tmp_sftmax)  # [16, numData]
         # print(np.shape(self.x_sftmax))
         self.A_abs = torch.abs(self.A)
         self.A_sm = self.A_abs / torch.sum(self.A_abs)
@@ -40,5 +44,5 @@ class TL_NN(nn.Module):
         self.weisum = torch.sum(self.A_sm_nonzr * self.x_sftmax, 0)
         self.xrtn = torch.sum(self.wsx, 0) / self.weisum
         # print(np.shape(self.xrtn))
-        print([self.t, self.A, self.b])
+        print([self.t, self.A, self.b, self.t2, self.b2])
         return self.xrtn
